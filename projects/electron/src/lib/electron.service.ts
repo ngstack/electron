5a5c5ee8db6edaa185771fc8dd61211937562c58
Denys Vuika <denys.vuika@gmail.com>
2019-08-12 16:36:34 +0100

    update angular libs

--- projects/electron/src/lib/electron.service.ts
+++ projects/electron/src/lib/electron.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NgZone } from '@angular/core';
-import { IpcRenderer, IpcRendererEvent } from 'electron';
+import { IpcRenderer } from 'electron';
 
 declare const window: any;
 
@@ -31,16 +31,13 @@ export class ElectronService {
     if (!this.ipcRenderer) {
       return;
     }
-    this.ipcRenderer.on(
-      channel,
-      (event: IpcRendererEvent, ...args: string[]) => {
-        this.zone.run(() => {
-          if (listener) {
-            listener(event, ...args);
-          }
-        });
-      }
-    );
+    this.ipcRenderer.on(channel, (event: any, ...args: string[]) => {
+      this.zone.run(() => {
+        if (listener) {
+          listener(event, ...args);
+        }
+      });
+    });
   }
 
   send(channel: string, ...args): void {
